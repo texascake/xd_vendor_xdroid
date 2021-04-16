4326e780f1fb82da34694aa5612d49c4adfd7e77
Danny Lin <danny@kdrag0n.dev>
2021-04-15 22:36:39 -0700

    vendor: Spoof build fingerprint for Google Play Services [2/2]
    SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    matches that of the device's stock OS, which has passed CTS testing.
    Spoof the fingerprint for Google Play Services to help pass SafetyNet.
    
    NB: This code is under the gmscompat package, but it does not depend on
    any code from gmscompat.
    
    arrow edits:
     - Correct prop name for our logic. We are planning use fp for different devices (like old oreo or follow new Pixel's).
     - Use PRODUCT_SYSTEM_DEFAULT_PROPERTIES for avoid error: ADDITIONAL_SYSTEM_PROPERTIES must not set before here.
     - Spoof dipper oreo fp
     - To override fp for GMS, add in device tree:
    PRODUCT_OVERRIDE_GMS_FINGERPRINT := your fingerprint
    
    Signed-off-by: palaych <palaych@arrowos.net>
    Change-Id: I862487adf736f499802b345b0044fb34ac204dd6
    Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

--- config/xd_props.mk
+++ config/xd_props.mk
@@ -45,3 +45,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Disable RescueParty
 PRODUCT_PRODUCT_PROPERTIES += \
         persist.sys.disable_rescue=true
+
+# Spoof fingerprint for Google Play Services and SafetyNet
+ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
+endif
