7608f1a16100d36a9e1e07e84b8b80434a90b8d9
Rahmad Adi Pratama <OhMyVenyx@proton.me>
2023-06-21 21:19:44 +0000

    xdroid: CAF > CLO
    Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
-export CAF_REVISION_TAG=$(grep "default  revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
+export CLO_REVISION_TAG=$(grep "default  revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
 
 # Welcome Msg
 echo -e ==========================================
 echo 	 xd. xdroidOS Build Environtment
 echo -e ==========================================
 echo -e "xd. Revision: rev1.8.redwhite"
-echo -e "CAF Revision: $CAF_REVISION_TAG"
+echo -e "CLO Revision: $CLO_REVISION_TAG"
 
 # SDClang Environment Variables
 export SDCLANG_CONFIG=$(pwd)/vendor/xdroid/sdclang/sdclang.json
